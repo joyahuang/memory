89d885709bc02f13061c035aca0ad4fc6a654267
Zhuoya Huang <zhuoyahuang@Zhuoyas-MacBook-Pro.local>
2021-11-07 20:13:28 -0800

    mean finish

---
+++ client/src/components/Posts/Posts.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { Grid, CircularProgress } from "@material-ui/core";
+import { useSelector } from "react-redux";
+import Post from "./Post/Post";
+import useStyles from "./style";
+const Posts = ({ setCurrentId }) => {
+  const classes = useStyles();
+  const posts = useSelector((state) => state.posts);
+  console.log(posts);
+  return !posts.length ? (
+    <CircularProgress></CircularProgress>
+  ) : (
+    <Grid
+      className={classes.container}
+      container
+      alighitems="strech"
+      spacing={3}
+    >
+      {posts.map((post) => (
+        <Grid key={post.id} item xs={12} sm={6}>
+          <Post post={post} setCurrentId={setCurrentId}></Post>
+        </Grid>
+      ))}
+    </Grid>
+  );
+};
+export default Posts;
